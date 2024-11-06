ea2bfac0249255b9a393bb2dfdd3dc22177d8b24
ElizabethGlebezdova <elizabeth.glebezdova@yandex.ru>
2024-11-06 19:33:56 +0300

    Исправлена ошибка Данечка

---
+++ app/main.py
@@ -0,0 +1,39 @@
+from contextlib import asynccontextmanager
+
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from starlette.requests import Request
+from starlette.responses import HTMLResponse
+from starlette.templating import Jinja2Templates
+
+from .database import create_table, delete_tables
+from .routes import router
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await delete_tables()
+    print("База очищена")
+    await create_table()
+    print("База готова")
+    yield
+    print("Выключение")
+
+app = FastAPI(lifespan=lifespan)
+templates = Jinja2Templates(directory="templates")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.get("/", response_class=HTMLResponse)
+async def read_index(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+
+app.include_router(router)
---
+++ app/schemas.py
@@ -0,0 +1,18 @@
+from pydantic import BaseModel
+from datetime import datetime
+
+
+class ReservationCreate(BaseModel):
+    guest_count: int
+    reservation_date: datetime
+    table_number: str
+
+
+class ReservationResponse(BaseModel):
+    id: int
+    guest_count: int
+    reservation_date: datetime
+    table_number: str
+
+    class Config:
+        orm_mode = True
