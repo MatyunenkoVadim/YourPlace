68504f0bdfbd25e65be50db7c7b30b5f52306e2b
efremdana <efremenkodana@gmail.com>
2024-11-16 20:24:34 +0300

    Настройка alembic

--- core/models/model.py
+++ core/models/model.py
@@ -1,27 +1,7 @@
-from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
-from sqlalchemy.orm import sessionmaker, DeclarativeBase, Mapped, mapped_column
-
-engine = create_async_engine(
-    "sqlite+aiosqlite:///YourPlace.db"
-)
-
-async_session = sessionmaker(
-    engine, expire_on_commit=False, class_=AsyncSession
-)
-
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 
 class Model(DeclarativeBase):
     __abstract__ = True
 
     id: Mapped[int] = mapped_column(primary_key=True)
-
-
-async def create_table():
-    async with engine.begin() as conn:
-        await conn.run_sync(Model.metadata.create_all)
-
-
-async def delete_tables():
-    async with engine.begin() as conn:
-        await conn.run_sync(Model.metadata.drop_all)
         
