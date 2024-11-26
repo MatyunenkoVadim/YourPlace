c4dc105b1abbf981798e8c4c33e8f840a10d02bf
ElizabethGlebezdova <elizabeth.glebezdova@yandex.ru>
2024-11-26 22:37:59 +0300

    Исправлена проблема с оформлением страниц

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./templates/**/*.html",
+    "./resources/templates/**/*.html",
   ],
   theme: {
     extend: {},
