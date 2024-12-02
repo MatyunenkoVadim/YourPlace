0ed3a11f3638a6eec40fd90a0da4788ca8f75568
ElizabethGlebezdova <elizabeth.glebezdova@yandex.ru>
2024-12-02 22:39:45 +0300

    Исправлена проблема с кодировкой

--- app/customStaticFiles.py
+++ app/customStaticFiles.py
@@ -4,6 +4,8 @@ from starlette.staticfiles import StaticFiles
 class CustomStaticFiles(StaticFiles):
     async def get_response(self, path: str, scope):
         response = await super().get_response(path, scope)
-        if path.endswith(".js"):
+        if path.endswith(".html"):
+            response.headers["Content-Type"] = "text/html; charset=utf-8"
+        elif path.endswith(".js"):
             response.headers["Content-Type"] = "application/javascript"
         return response
