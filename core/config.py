74a7ac1f623f3861640ebba454c9014217635819
efremdana <efremenkodana@gmail.com>
2024-12-04 21:24:33 +0300

    Добавил token secret

--- core/config.py
+++ core/config.py
@@ -10,6 +10,8 @@ class AuthJWT(BaseModel):
     public_key_path: Path = BASE_DIR / "core" / "secure" / "key" / "jwt-public.pem"
     algorithm: str = "RS256"
     access_token_expire_minutes: int = 5
+    reset_password_token_secret: str = '3543b40a41ba6524542afae200638366053d71f48e17e79d01d6bd257916bb29'
+    verification_token_secret: str = '529164a9965294119bb866978749cf0f157ddf2edba8a6245a6081f95eafd7d1'
 
 class DBSettings(BaseModel):
     db_url: str = "sqlite+aiosqlite:///YourPlace.db"
