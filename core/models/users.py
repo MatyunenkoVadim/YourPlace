48333a756e56bb4033ed10929b6f2cea6ba3de23
efremdana <efremenkodana@gmail.com>
2024-12-04 19:57:39 +0300

    Создал User model

--- core/models/users.py
+++ core/models/users.py
@@ -2,6 +2,28 @@ from sqlalchemy.orm import Mapped, mapped_column
 
 from .model import Model
 
+from typing import TYPE_CHECKING
+
+from fastapi_users_db_sqlalchemy import (
+    SQLAlchemyBaseUserTable,
+    SQLAlchemyUserDatabase,
+)
+# TODO: Решить вопрос с UserIdType
+# from core.types.user_id import UserIdType
+# from .mixins.id_int_pk import IdIntPkMixin
+
+if TYPE_CHECKING:
+    from sqlalchemy.ext.asyncio import AsyncSession
+
+# TODO: Создать класс IdIntPkMixin
+class UserDB(Model, SQLAlchemyBaseUserTable[int]):
+# class UserDB(Model, SQLAlchemyBaseUserTable[UserIdType]):
+    __tablename__ = "users"
+
+    @classmethod
+    def get_db(cls, session: "AsyncSession"):
+        return SQLAlchemyUserDatabase(session, cls)
+
 
 class User(Model):
     __tablename__ = "users"
