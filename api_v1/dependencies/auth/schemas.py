00823b3f265b55a4cfa860f92ad602f800d2a148
efremdana <efremenkodana@gmail.com>
2024-12-07 19:34:27 +0300

    update users schemas

--- api_v1/dependencies/auth/schemas.py
+++ api_v1/dependencies/auth/schemas.py
@@ -1,29 +1,18 @@
 from typing import Optional
 
 from fastapi_users import schemas
-from pydantic import BaseModel
-
-# from core.types.user_id import UserIdType
 
 
 class UserRead(schemas.BaseUser[int]):
-    username: str
     phone: str | None
     fullname: str | None
 
 
 class UserCreate(schemas.BaseUserCreate):
-    username: str
     phone: Optional[str] = None
     fullname: Optional[str] = None
 
 
 class UserUpdate(schemas.BaseUserUpdate):
-    username: Optional[str] = None
     phone: Optional[str] = None
     fullname: Optional[str] = None
-
-
-class UserRegisteredNotification(BaseModel):
-    user: UserRead
-    ts: int
