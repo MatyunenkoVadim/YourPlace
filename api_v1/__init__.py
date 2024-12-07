378788197900f0922270f7f89da9ad229e66a003
efremdana <efremenkodana@gmail.com>
2024-12-07 19:31:06 +0300

    Delete old api_user router

--- api_v1/__init__.py
+++ api_v1/__init__.py
@@ -1,13 +1,10 @@
 from fastapi import APIRouter
 from .reservations.view import router as router_reservation
-# from .users.view import router as router_users
 from .users.auth import router as auth_router
 from .users.user_info import router as user_router
 
 router = APIRouter()
 
 router.include_router(router=router_reservation, prefix="/reservation")
-# TODO: Delete api_user router
-# router.include_router(router=router_users, prefix="/user")
 router.include_router(auth_router)
 router.include_router(user_router)
