0f4174ad05778e21c00ddf245b3405bc818093db
ElizabethGlebezdova <elizabeth.glebezdova@yandex.ru>
2024-12-10 21:21:17 +0300

    Сделано красивое оформление навбара

--- resources/src/components/Header.jsx
+++ resources/src/components/Header.jsx
@@ -1,12 +1,33 @@
-import React from "react";
-import '../index.css';
+import React, { useState } from "react";
+import "../index.css";
+import Logo from "../images/logo.svg";
 
 const Header = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
-    <div className="header">
-      <div className="header-title">Свое Место</div>
+    <header className="header">
+      <div className="header-logo">
+        <img src={Logo} alt="Своё Место" className="logo" />
+      </div>
+
+      <button className="menu-toggle" onClick={toggleMenu}>
+        ☰
+      </button>
+
+      <nav className={`nav-links ${menuOpen ? "nav-open" : ""}`}>
+        <a href="/" className="nav-link">Главная</a>
+        <a href="/reservations" className="nav-link">Мои бронирования</a>
+        <a href="/contacts" className="nav-link">Контакты</a>
+        <button className="mobile-login-button">Войти</button>
+      </nav>
+
       <button className="login-button">Войти</button>
-    </div>
+    </header>
   );
 };
 
